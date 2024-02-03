50e266ad456a0000362f0efbbdb98a60c3b045a9
kyubuns <kyubuns@gmail.com>
2024-02-03 17:43:52 +0900

    init

---
+++ Source/CommonUITest/CommonUITestGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CommonUITestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COMMONUITEST_API ACommonUITestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
